f270a67e58306807e36d70fb10d98967b45425ac
Rudra <158558490+Omkarrudra09@users.noreply.github.com>
2024-02-15 12:18:54 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -30,10 +30,10 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_8"], callback_data="gib_source")
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
         ],
     ]
     return buttons
